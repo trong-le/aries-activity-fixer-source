1af7f2a4811ddca73133912526e300e0ef6a5f25
trong-le <trong.p.le.92@gmail.com>
2016-07-05 15:02:20 -0400

    Working version with added formatting of exchange rates

---
+++ test/test.config.js
@@ -0,0 +1,4 @@
+export default {
+	givenDate: '2000-01-03',
+	base: 'USD'
+};
---
+++ test/test.js
@@ -0,0 +1,21 @@
+import test from 'blue-tape';
+import FixerSource from '../lib/index.js';
+import config from './test.config';
+
+// example - make sure configuration is the same
+test('proper configuration', t => {
+	const activity = new FixerSource();
+	t.equal(FixerSource.props.name, require('../package.json').name);
+	t.equal(FixerSource.props.version, require('../package.json').version);
+	t.end();
+});
+
+test('test getExchangeRateData method', async (t) => {
+	const source = new FixerSource();
+	const data = await source.getExchangeRateData(config);
+});
+
+test('oauth onTaskCopy', async (t) => {
+	const source = new FixerSource();
+	const data = await source.onTaskCopy(config);
+});
